7a20343bdb36445783dab316e33f2c70d09b6415
alnomancs <alnoman.cs@gmail.com>
2022-07-02 04:17:59 +0100

    heroku deploy

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const {
+  MongoClient,
+  ServerApiVersion,
+  ClientSession,
+  ObjectId,
+} = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -9,8 +14,7 @@ const port = process.env.PORT || 5005;
 app.use(express.json());
 app.use(cors());
 
-const uri =
-  "mongodb+srv://admin:admin@cluster0.yf4we.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yf4we.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -20,17 +24,53 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const tasksDb = client.db("task-management").collection("tasks");
+    const tasksCollections = client.db("task-management").collection("tasks");
+
+    app.post("/task", async (req, res) => {
+      const id = req.query._id;
+      const status = req.query.status;
+
+      console.log(id, status);
+
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: status,
+        },
+      };
+
+      const result = await tasksCollections.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log(result);
+      res.send(result);
+    });
 
     app.get("/tasks", async (req, res) => {
-      const tasks = await tasksDb.find().toArray();
+      const query = { status: "0" };
+
+      const tasks = await tasksCollections.find(query).toArray();
+
+      console.log(tasks);
+      res.send(tasks);
+    });
+    app.get("/tasksComplete", async (req, res) => {
+      const query = { status: "1" };
+
+      const tasks = await tasksCollections.find(query).toArray();
+
+      console.log(tasks);
       res.send(tasks);
     });
 
     //insert reviews
     app.post("/addTask", async (req, res) => {
       const task = req.body;
-      const result = await tasksDb.insertOne(task);
+      console.log(task);
+      const result = await tasksCollections.insertOne(task);
       console.log(result);
       res.send(result);
     });
